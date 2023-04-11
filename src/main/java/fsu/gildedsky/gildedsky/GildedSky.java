1fa798e1f4b91ec2f24a26b6dcd9166919ba1e6b
Lucian Rectanus <luc14n@users.noreply.github.com>
2023-04-11 15:06:56 -0400

    Lucian 01 Merge with Master (#1)
    * Testing to make sure everything works
    
    * Unfinished update - 4/8/2023
    
    DONE:
    Created creative mod tab and added item to it
    Imported all old item .json files
    Imported all old textures
    
    * Create README.md
    
    * Added Extra Test Item to creative mod tab
    
    * Started import of Items
    
    Imported Skymetal Items and established organization for blocks and items.
    
    * Started import of Items
    
    Imported Skymetal Items and established organization for blocks and items.

--- src/main/java/fsu/gildedsky/gildedsky/GildedSky.java
+++ src/main/java/fsu/gildedsky/gildedsky/GildedSky.java
@@ -1,32 +1,23 @@
 package fsu.gildedsky.gildedsky;
 
 import com.mojang.logging.LogUtils;
-import net.minecraft.client.Minecraft;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.Material;
+import fsu.gildedsky.gildedsky.item.ModCreativeModeTab;
+import fsu.gildedsky.gildedsky.item.ModItems;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.CreativeModeTabEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
+// TODO: Make ModBlocks.java and import all of the blocks into the system.
+// TODO: Find and import crafting recipes, change crafting recipes for Gilded block to be block plus gold nuggets.
+// TODO: Find out how to test the game
+
 @Mod(GildedSky.MOD_ID)
 public class GildedSky {
 	public static final String MOD_ID = "gildedsky";
@@ -34,21 +25,49 @@ public class GildedSky {
 
 	public GildedSky() {
 		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+		ModItems.register(modEventBus);
+
 		modEventBus.addListener(this::commonSetup);
 		MinecraftForge.EVENT_BUS.register(this);
+
+		modEventBus.addListener(this::addCreative);
 	}
+
+	// This is a test to make sure everything works properly!
 
 	private void commonSetup(final FMLCommonSetupEvent event) {
-		
+
 	}
 
-	// You can use EventBusSubscriber to automatically register all static methods
-	// in the class annotated with @SubscribeEvent
+	private void addCreative(CreativeModeTabEvent.BuildContents event) {
+		if (event.getTab() == ModCreativeModeTab.GILDEDSKY_TAB) {
+			// Test Items
+			event.accept(ModItems.TESTITEM);
+
+			// Sky Metal
+			event.accept(ModItems.SKYMETAL_INGOT);
+				// Tools && Weapons
+			event.accept(ModItems.SKYMETAL_PICKAXE);
+			event.accept(ModItems.SKYMETAL_AXE);
+			event.accept(ModItems.SKYMETAL_SHOVEL);
+			event.accept(ModItems.SKYMETAL_HOE);
+			event.accept(ModItems.SKYMETAL_SWROD);
+				// Armor
+			event.accept(ModItems.SKYMETAL_ARMOR_HELMET);
+			event.accept(ModItems.SKYMETAL_ARMOR_CHESTPLATE);
+			event.accept(ModItems.SKYMETAL_ARMOR_LEGGINGS);
+			event.accept(ModItems.SKYMETAL_ARMOR_BOOTS);
+		}
+	}
+
+	// You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
 	@Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 	public static class ClientModEvents {
 		@SubscribeEvent
 		public static void onClientSetup(FMLClientSetupEvent event) {
-			
+
 		}
 	}
 }
+
---
+++ src/main/java/fsu/gildedsky/gildedsky/item/ModCreativeModeTab.java
@@ -0,0 +1,23 @@
+package fsu.gildedsky.gildedsky.item;
+
+import fsu.gildedsky.gildedsky.GildedSky;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.event.CreativeModeTabEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+public class ModCreativeModeTab {
+
+    //@Mod.EventBusSubscriber(modid = GildedSky.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static CreativeModeTab GILDEDSKY_TAB;
+
+    @SubscribeEvent
+    public static void registerCreativeModeTabs(CreativeModeTabEvent.Register event) {
+        GILDEDSKY_TAB = event.registerCreativeModeTab(new ResourceLocation(GildedSky.MOD_ID, "gildedsky_tab"),
+                builder -> builder.icon(
+                        () -> new ItemStack(ModItems.SKYMETAL_INGOT.get())).title(Component.literal("GildedSky Tab")).build()
+                );
+    }
+}
