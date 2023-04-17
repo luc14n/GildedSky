19e2d8b4870adcc1608be4a4c39399aeb56e94aa
Eric Rinard <cpu@127.0.0.1>
2023-04-17 19:37:28 -0400

    Make portal light from flint and steel

--- src/main/java/fsu/gildedsky/gildedsky/item/impl/PurpleCarrotItem.java
+++ src/main/java/fsu/gildedsky/gildedsky/item/impl/PurpleCarrotItem.java
@@ -48,26 +48,4 @@ public class PurpleCarrotItem extends Item {
             }
         return super.onItemUseFirst(stack,context);
     }
-    //TODO move to different item
-    @Override
-    public InteractionResult useOn(UseOnContext context) {
-        Player entity = context.getPlayer();
-        BlockPos pos = context.getClickedPos().relative(context.getClickedFace());
-        ItemStack itemstack = context.getItemInHand();
-        Level world = context.getLevel();
-        if (!entity.mayUseItemAt(pos, context.getClickedFace(), itemstack)) {
-            return InteractionResult.FAIL;
-        } else {
-            int x = pos.getX();
-            int y = pos.getY();
-            int z = pos.getZ();
-            boolean success = false;
-            if (world.isEmptyBlock(pos)) {
-                SkyPortalBlock.portalSpawn(world, pos);
-                itemstack.hurtAndBreak(1, entity, c -> c.broadcastBreakEvent(context.getHand()));
-                success = true;
-            }
-            return success ? InteractionResult.SUCCESS : InteractionResult.FAIL;
-        }
-    }
 }
