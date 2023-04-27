a977214fd89a9e643f7aa185e3ae67d54bad91a4
Eric Rinard <cpu@127.0.0.1>
2023-04-27 14:37:02 -0400

    Add village trade stub

---
+++ src/main/java/fsu/gildedsky/gildedsky/ModEvents.java
@@ -0,0 +1,29 @@
+package fsu.gildedsky.gildedsky;
+
+import fsu.gildedsky.gildedsky.item.ModItems;
+import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
+import net.minecraft.world.entity.npc.VillagerProfession;
+import net.minecraft.world.entity.npc.VillagerTrades;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.trading.MerchantOffer;
+import net.minecraftforge.event.village.VillagerTradesEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+import java.util.List;
+@Mod.EventBusSubscriber(modid = GildedSky.MOD_ID)
+public class ModEvents {
+    @SubscribeEvent
+    public static void addCustomTrades(VillagerTradesEvent event) {
+        if(event.getType() == VillagerProfession.FARMER) {
+            Int2ObjectMap<List<VillagerTrades.ItemListing>> trades = event.getTrades();
+            ItemStack stack = new ItemStack(ModItems.PURPLE_CARROT.get(), 12);
+            int villagerLevel = 1;
+
+            trades.get(villagerLevel).add((trader, rand) -> new MerchantOffer(
+                    new ItemStack(Items.EMERALD, 1),
+                    stack,10,8,0.02F));
+        }
+    }
+}
